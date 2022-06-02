e596649fac90cc304d36128d104d6dd006dd906b
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-06-01 22:40:31 -0400

    updated typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,7 @@
-const { gql } = require('apollo-server-express');
+const {gql} = require("apollo-server-express");
 
 const typeDefs = gql`
-  type Profile {
+  type Reader {
     _id: ID
     name: String
     email: String
@@ -15,19 +15,15 @@ const typeDefs = gql`
   }
 
   type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
-    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
-    me: Profile
+    readers: [Reader]!
+    reader(readerId: ID!): Reader
+    me: Reader
   }
 
   type Mutation {
-    addProfile(name: String!, email: String!, password: String!): Auth
+    addReader(name: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile: Profile
-    removeSkill(skill: String!): Profile
+    removeReader: Reader
   }
 `;
 
