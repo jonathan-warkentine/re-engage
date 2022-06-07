4fc519fbf756c52e6260ce017b7e8ace07866b44
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-06-07 15:11:20 -0400

    set display:false to all new type Word objects, removed addSplitBody mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -71,7 +71,6 @@ const typeDefs = gql`
     updateReader(_id: ID!, name: String, email: String, password: String, screenName: String): Reader
     updatePassage(_id: ID!, title: String, fullBody: String): Passage
     addPassage(title: String, providedBy: ID, fullBody: String): Passage
-    addSplitBody(title: String, providedBy: ID, fullBody: String, splitBody: [SentenceInput]): Passage
     deletePassage(_id: ID!): Passage
   }
 `;
